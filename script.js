cc324250a9d7e5d424d65fb787951edd3d2a4cf7
Rahul Gupta <88659833+Rahulgupta02@users.noreply.github.com>
2023-08-18 15:08:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer","Designer", "Freelancer"],
+        strings: ["Developer","Designer", "Coder", "Freelancer"],
         typeSpeed: 80,
         backSpeed: 45,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Designer", "Freelancer"],
+        strings: ["Developer", "Designer", "Coder", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
